e7c2f9fa4b4c3e08819a08af217b008da08870bc
Patrick Kelly <pmkelly4444@gmail.com>
2022-08-08 09:17:24 -0400

    test script gets jwt

--- src/bin/test_api.rs
+++ src/bin/test_api.rs
@@ -1,5 +1,37 @@
-use http::{Request, Response};
-use std::io::stdin;
+use std::{collections::HashMap, io::stdin};
+
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct LoginResponse {
+    pub token: String,
+}
+
+pub type Result<T, CustomErr> = std::result::Result<T, CustomErr>;
+
+fn get_token(username: &str, password: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let mut map = HashMap::new();
+    map.insert("username", username);
+    map.insert("password", password);
+    let client = reqwest::blocking::Client::new();
+    let resp = client
+        .post("http://localhost:8000/login")
+        .json(&map)
+        .send()?;
+
+    if resp.status().is_success() {
+        println!("successfully retieved JWT");
+        let res: LoginResponse = resp.json()?;
+        return Ok(res.token);
+    } else {
+        println!("Unable to log in with the provided credentials");
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Unable to log in with the provided credentials",
+        )
+        .into());
+    }
+}
 
 #[allow(dead_code)]
 fn main() {
@@ -11,22 +43,9 @@ fn main() {
     let mut password = String::new();
     stdin().read_line(&mut password).unwrap();
     let password = &password[..(password.len() - 1)]; // Drop the newline character
-
-    let mut request = Request::Builder::new()
-        .method("POST")
-        .body(format!("username={}&password={}", username, password))
-        .unwrap();
-    request
-        .uri("http://localhost:8000/login")
-        .header("Content-Type", "application/json");
-
-    let response = send(request.body(()).unwrap());
-
-    fn send(req: Request<()>) -> Response<()> {
-        let mut response = req.send().unwrap();
-        let body = response.body().unwrap();
-        let body = body.into_string().unwrap();
-        println!("{}", body);
-        response
+    match get_token(username, password) {
+        Ok(token) => println!("{}", token),
+        Err(e) => println!("{}", e),
     }
+    return;
 }
