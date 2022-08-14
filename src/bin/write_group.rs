22a87ce00782b12e8c896de6876c0f67ff923265
Patrick Kelly <pmkelly4444@gmail.com>
2022-08-14 11:19:05 -0400

    write group now lets you add a friend to the group

--- src/bin/write_group.rs
+++ src/bin/write_group.rs
@@ -2,7 +2,7 @@ extern crate diesel;
 use self::diesel::prelude::*;
 
 use rusty_chat::db::establish_connection;
-use rusty_chat::group::handlers::create_group;
+use rusty_chat::group::handlers::{add_members_to_group, create_group};
 use rusty_chat::user::models::users::dsl::*;
 
 use std::io::stdin;
@@ -26,9 +26,17 @@ fn main() {
     let mut group_name = String::new();
     stdin().read_line(&mut group_name).unwrap();
     let group_name = &group_name[..(group_name.len() - 1)]; // Drop the newline character
-    let connection = establish_connection();
+
+    println!("What user id you like to add to your group?");
+    let mut new_user_id = String::new();
+    stdin().read_line(&mut new_user_id).unwrap();
+    let new_user_id = &new_user_id[..(new_user_id).len() - 1]; // Drop the newline character
+    let new_user_id_int = new_user_id.parse::<i32>().unwrap();
+    let member_ids: Vec<i32> = [user_id, new_user_id_int].to_vec();
 
     let group = create_group(&connection, &user_id, group_name);
+    let group_id = &group.id;
+    let _members_added = add_members_to_group(&connection, group_id, member_ids);
     println!(
         "Successfully created group {} with ID {} ",
         group.name, group.id
